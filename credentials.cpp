4bb60bb87573b4c38210e0c11b560068c2741402
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-12-20 20:12:05 +0530

    Delete credentials.cpp

--- credentials.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-
-using std::string;
-
- string client_id = "w49MIL3L";
- string client_secret = "BVQhm0_bvx1PCRct0cRrJSQmfVi8nXI2jjsW8WHEobo";
- string base_url = "https://test.deribit.com/api/v2";
- string accessToken;
